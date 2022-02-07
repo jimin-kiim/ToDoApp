f6ec54cbe03837df9b2041de97d03b821ba1ba43
Jimin Kim <0305jimin@gmail.com>
2022-02-07 16:12:37 +0900

    feat: changing mode between 'work' and 'travel'

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    // TouchableHighlight,
+    // TouchableWithoutFeedback,
+    // Pressable,
+    TextInput,
+} from "react-native";
+import { theme } from "./colors";
+import React, { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [working, setWorking] = useState(true);
+    const [text, setText] = useState("");
+    const travel = () => setWorking(false);
+    const work = () => setWorking(true);
+    const onChangeText = (payload) => {
+        setText(payload);
+    };
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.header}>
+                <TouchableOpacity onPress={work}>
+                    <Text
+                        style={{
+                            ...styles.btnText,
+                            color: working ? "white" : theme.grey,
+                        }}
+                    >
+                        Work
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={travel}>
+                    <Text
+                        style={{
+                            ...styles.btnText,
+                            color: !working ? "white" : theme.grey,
+                        }}
+                    >
+                        Travel
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        paddingHorizontal: 20,
+    },
+    header: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        marginTop: 100,
+    },
+    btnText: {
+        fontSize: 45,
+        fontWeight: "600",
+    },
 });
